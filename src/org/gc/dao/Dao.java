b0260c08a3439f919217ddffb7343681a35bb564
Gabriel Chiu <gchiu-2026118@kinal.edu.gt>
2026-09-24 11:44:31 -0600

    agregue el javadoc a Dao.java

--- src/org/gc/dao/Dao.java
+++ src/org/gc/dao/Dao.java
@@ -1,5 +1,18 @@
 
 package org.gc.dao;
+ 
+/**
+* Contrato DAO genérico que actúa como interfaz base centralizada para la
+* gestión de persistencia de datos en el sistema, extendiendo las operaciones
+* estándar definidas en {@link Crud}.
+* @param <T> El tipo de entidad o modelo de dominio que maneja el DAO.
+* @param <K> El tipo de dato correspondiente a la clave primaria (ID) de la entidad.
+* @author Gabriel Chiu
+* @version 1.0.0
+* @see org.gc.dao.Crud
+*/
 
 public interface Dao<T, K> extends crud<T, K> {
+ 
 }
+ 
