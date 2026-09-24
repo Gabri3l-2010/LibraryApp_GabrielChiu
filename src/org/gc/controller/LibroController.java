87059f3a42a212ed58ca3aa729fa6b6124aff7e2
gabriel chiu <gchiu-2026118@kinal.edu.gt>
2026-09-24 16:33:33 -0600

    docs(javadoc): documentar LibroController

--- src/org/gc/controller/LibroController.java
+++ src/org/gc/controller/LibroController.java
@@ -3,7 +3,17 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
 
+/**
+ * Controlador para gestionar la vista de Libro.
+ * @author Gabriel Chiu
+ * @version 1.0.0
+ * @see org.gc.view.fxml.LibroView
+ */
 public class LibroController implements Initializable {
+    /**
+     * @param url Ubicacion
+     * @param rb Recursos
+     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // Inicializar
