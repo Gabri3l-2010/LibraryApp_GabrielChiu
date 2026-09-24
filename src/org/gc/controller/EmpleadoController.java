4c7a46f5e8f5a71649277deca891bc961034c343
gabriel chiu <gchiu-2026118@kinal.edu.gt>
2026-09-24 16:33:19 -0600

    docs(javadoc): documentar EmpleadoController

--- src/org/gc/controller/EmpleadoController.java
+++ src/org/gc/controller/EmpleadoController.java
@@ -3,7 +3,17 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
 
+/**
+ * Controlador para gestionar la vista de Empleado.
+ * @author Gabriel Chiu
+ * @version 1.0.0
+ * @see org.gc.view.fxml.EmpleadoView
+ */
 public class EmpleadoController implements Initializable {
+    /**
+     * @param url Ubicacion
+     * @param rb Recursos
+     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // Inicializar
