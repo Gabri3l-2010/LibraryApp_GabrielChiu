285ad397128a70aa8021d5e47fe60898890c52e4
gabriel chiu <gchiu-2026118@kinal.edu.gt>
2026-09-24 16:33:01 -0600

    docs(javadoc): documentar CajeroController

--- src/org/gc/controller/CajeroController.java
+++ src/org/gc/controller/CajeroController.java
@@ -3,7 +3,17 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
 
+/**
+ * Controlador para gestionar la vista de Cajero.
+ * @author Gabriel Chiu
+ * @version 1.0.0
+ * @see org.gc.view.fxml.CajeroView
+ */
 public class CajeroController implements Initializable {
+    /**
+     * @param url Ubicacion
+     * @param rb Recursos
+     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // Inicializar
