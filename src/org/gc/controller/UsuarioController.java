fe018d0aa851b1af56405809ea025b874e3e3478
gabriel chiu <gchiu-2026118@kinal.edu.gt>
2026-09-24 16:33:44 -0600

    docs(javadoc): documentar UsuarioController

--- src/org/gc/controller/UsuarioController.java
+++ src/org/gc/controller/UsuarioController.java
@@ -3,7 +3,17 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
 
+/**
+ * Controlador para gestionar la vista de Usuario.
+ * @author Gabriel Chiu
+ * @version 1.0.0
+ * @see org.gc.view.fxml.UsuarioView
+ */
 public class UsuarioController implements Initializable {
+    /**
+     * @param url Ubicacion
+     * @param rb Recursos
+     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // Inicializar
