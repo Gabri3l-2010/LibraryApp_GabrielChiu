dd7937805088616c5746b33b705458eb42777524
gabriel chiu <gchiu-2026118@kinal.edu.gt>
2026-09-24 16:32:57 -0600

    docs(javadoc): documentar AutorLibroController

--- src/org/gc/controller/AutorLibroController.java
+++ src/org/gc/controller/AutorLibroController.java
@@ -3,7 +3,17 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
 
+/**
+ * Controlador para gestionar la vista de AutorLibro.
+ * @author Gabriel Chiu
+ * @version 1.0.0
+ * @see org.gc.view.fxml.AutorLibroView
+ */
 public class AutorLibroController implements Initializable {
+    /**
+     * @param url Ubicacion
+     * @param rb Recursos
+     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // Inicializar
