f9058b830dc25c3040e71e38383f4ec1aecd95f5
gabriel chiu <gchiu-2026118@kinal.edu.gt>
2026-09-24 16:33:44 -0600

    feat(controller): agregar clase UsuarioController

---
+++ src/org/gc/controller/UsuarioController.java
@@ -0,0 +1,11 @@
+﻿package org.gc.controller;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+public class UsuarioController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // Inicializar
+    }
+}
