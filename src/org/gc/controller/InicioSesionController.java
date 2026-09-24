a2f33d14f32f838519955e367071eb1c02fd88ac
gabriel chiu <gchiu-2026118@kinal.edu.gt>
2026-09-24 16:33:26 -0600

    docs(javadoc): documentar InicioSesionController

--- src/org/gc/controller/InicioSesionController.java
+++ src/org/gc/controller/InicioSesionController.java
@@ -3,7 +3,17 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
 
+/**
+ * Controlador para gestionar la vista de InicioSesion.
+ * @author Gabriel Chiu
+ * @version 1.0.0
+ * @see org.gc.view.fxml.InicioSesionView
+ */
 public class InicioSesionController implements Initializable {
+    /**
+     * @param url Ubicacion
+     * @param rb Recursos
+     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // Inicializar
