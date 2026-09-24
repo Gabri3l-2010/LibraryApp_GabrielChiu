d47cb7c605cd1ef4ee8fd19c6c87aed81c5d747e
gabriel chiu <gchiu-2026118@kinal.edu.gt>
2026-09-24 16:33:04 -0600

    docs(javadoc): documentar CategoriaController

--- src/org/gc/controller/CategoriaController.java
+++ src/org/gc/controller/CategoriaController.java
@@ -3,7 +3,17 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
 
+/**
+ * Controlador para gestionar la vista de Categoria.
+ * @author Gabriel Chiu
+ * @version 1.0.0
+ * @see org.gc.view.fxml.CategoriaView
+ */
 public class CategoriaController implements Initializable {
+    /**
+     * @param url Ubicacion
+     * @param rb Recursos
+     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // Inicializar
